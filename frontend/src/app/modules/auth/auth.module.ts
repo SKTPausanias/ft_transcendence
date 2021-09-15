91ecb1f5de234b1e7a4f3050505f3248640269f5
jheras-f <photowifirnd@gmail.com>
2021-09-15 16:30:05 +0200

    Auth2factor divs

--- frontend/src/app/modules/auth/auth.module.ts
+++ frontend/src/app/modules/auth/auth.module.ts
@@ -6,7 +6,7 @@ import { RegistrationComponent } from './components/registration/registration.co
 import { AuthComponent} from './auth.component';
 import { LoadingComponent } from './components/loading/loading.component';
 import { FormsModule } from '@angular/forms';
-import { Auth2factorComponent } from './components/auth2factor/auth2factor.component'
+import { Auth2factorComponent, ResendBoxComponent } from './components/auth2factor/auth2factor.component'
 
 
 @NgModule({
@@ -16,7 +16,8 @@ import { Auth2factorComponent } from './components/auth2factor/auth2factor.compo
     RegistrationComponent,
     ConfirmationComponent,
     LoadingComponent,
-    Auth2factorComponent
+    Auth2factorComponent,
+    ResendBoxComponent
   ],
   imports: [
     CommonModule,
--- frontend/src/app/modules/auth/components/auth2factor/auth2factor.component.ts
+++ frontend/src/app/modules/auth/components/auth2factor/auth2factor.component.ts
@@ -7,31 +7,46 @@ import { CodeI } from '../../../../shared/interface/c2f';
 import { stringify } from '@angular/compiler/src/util';
 
 @Component({
+  selector: 'resend-box',
+  template: `<div #resend_msg>
+              <span>Your authentication code has been sent.</span>
+              <button type="button" class="" aria-label="Dismiss this message" (click)="onClickResendBox($event)"
+                name="X">
+                X
+              </button>
+            </div>`,
+  styleUrls: ['./auth2factor.component.css']
+})
+export class ResendBoxComponent {
+  @ViewChild('resend_msg') resendMsg: ElementRef;
+  constructor(private router: Router, private authService: AuthService) { }
+  onClickResendBox(event: Event) {
+    this.resendMsg.nativeElement.remove();
+  }
+}
+@Component({
   selector: 'app-auth2factor',
   templateUrl: './auth2factor.component.html',
   styleUrls: ['./auth2factor.component.css']
 })
 export class Auth2factorComponent implements OnInit {
   @ViewChild('parent') parentElement: ElementRef<HTMLInputElement>;
+  resendBox: ResendBoxComponent;
   user: UserI = this.sQuery.getUser();
   userEmail: string = "";
   c2f: CodeI = <CodeI>{};
   date_formated : string;
-
+  show : boolean = false;
   constructor(private sQuery: LocalStorageQueryService,
               private authService: AuthService,
               private route: Router) { }
-
   async ngOnInit(): Promise<void> {
     this.partialHide();
-    console.log("parentElement: ",this.parentElement);
     this.c2f = await this.authService.sendCode2Factor(this.user);
     this.date_formated =  this.toDateTime(this.c2f.expiration_time);
   }
-
   partialHide(): void {
     var i;
-
     this.userEmail += this.user.email[0];
     for (i = 1; this.user.email[i] != '@'; i++)
     {
@@ -46,12 +61,15 @@ export class Auth2factorComponent implements OnInit {
     if (ret)
       this.route.navigateByUrl('/');
   }
-
   async reSendCode(): Promise<void> {
     this.c2f = await this.authService.reSendCode2Factor(this.user);
     this.date_formated =  this.toDateTime(this.c2f.expiration_time);
+    console.log("parentElement: ",this.parentElement);
+    this.show = true;
+    //var node = document.createElement("div");
+    //node.textContent = "Re-send";
+    //this.parentElement.nativeElement.insertBefore(node, this.parentElement.nativeElement.firstChild);
   }
-
   toDateTime(secs : number): string {
     var str1 : string;
     var t = new Date(1970, 0, 1); // Epoch
