46c009282f98004b3258ad6422363393d0711bc3
jheras-f <photowifirnd@gmail.com>
2021-10-21 11:07:43 +0200

    fix showing pending invitations dashboard

--- frontend/src/app/home/content/dashboard/notifications/friend-invitation/friend-invitation.component.ts
+++ frontend/src/app/home/content/dashboard/notifications/friend-invitation/friend-invitation.component.ts
@@ -30,7 +30,7 @@ export class FriendInvitationComponent implements OnInit {
 		this.pos < 0 ? (this.pos = 0) : 0;
 		if (this.friendPreferneces.friend_invitation.length > 0)
 			return (this.friendPreferneces.friend_invitation[this.pos].nickname)
-		return ("EMTY");
+		return ("EMPTY");
 	}
 	async accept(){
 		const user = this.friendPreferneces.friend_invitation[this.pos];
