2fbb1ae46c56c34021977e3b58685857e9d96708
SKTPausanias <miguellaplanamarin@gmail.com>
2021-11-03 16:31:59 +0100

    Cmessajes.ts updated, modified date

--- frontend/src/app/shared/class/cMessages.ts
+++ frontend/src/app/shared/class/cMessages.ts
@@ -1,3 +1,4 @@
+import { DatePipe } from "@angular/common";
 import { MessagesI } from "../interface/iChat";
 import { UserPublicInfoI } from "../interface/iUserInfo";
 
@@ -17,6 +18,12 @@ export class Messages implements MessagesI{
     {
         this.message = value.message;
         this.msgDateString = new Date(value.date * 1000).toString();
+        this.msgDateString = this.msgDateString.substring(0, this.msgDateString.indexOf("GMT"));
+        //remove seconds from date
+        this.msgDateString = this.msgDateString.substring(0, this.msgDateString.lastIndexOf(":"));
+        // remove weekday from date
+        this.msgDateString = this.msgDateString.substring(this.msgDateString.indexOf(",") + 4);
+        console.log(this.msgDateString);
         this.date = value.date;
         this.user = value.user;
         this.owner = value.owner;
