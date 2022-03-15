a62590b5b3d7e2e47275796a9d330cf0b7ea0d51
photowifirnd <62238183+photowifirnd@users.noreply.github.com>
2022-03-15 13:11:02 +0100

    Delete testingChat directory

--- testingChat/backend/dist/events/events.gateway.d.ts
+++
@@ -1,7 +0,0 @@
-import { OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
-export declare class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
-    server: any;
-    handleConnection(client: any): Promise<void>;
-    handleDisconnect(client: any): Promise<void>;
-    handshake(client: any, message: any): Promise<void>;
-}
